9e18c336db3e29508570bcdbac80b43050c09681
Bonyonte <godfrey.magogo@student.moringaschool.comgit config --global user.name Bonyontegit branch -m maingit branch -M maingit remote add origin git@github.com:Bonyonte/ip_1.gitgit push -u origin maingit config --global user.email godfrey.magogo@student.moringaschool.com>
2022-07-18 13:16:31 +0300

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
@@ -8,12 +8,24 @@ console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [tasks, setTasks] = useState(TASKS);
+  const [category, setCategory] = useState("ALL");
+
+  function handleAddTask(newTask) {
+    setTasks([...tasks, newTask]);
+  }
+
+  function handleDeleteTask(deletedTaskText){
+    setTasks(tasks.filter((task) => task.text !== deletedTaskText));
+  }
+
+  const visibleTasks = tasks.filter((task) => category === "All" || task.category === category);
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <NewTaskForm categories={CATEGORIES.filter((cat) => cat !== "All")} 
+      onTaskFormSubmit= {handleAddTask} />
+      <TaskList onDeleteTask={handleDeleteTask} tasks={visibleTasks}/>
     </div>
   );
 }
